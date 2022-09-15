a2878455c9521f16b1050300838672737d53c901
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-15 15:45:19 -0400

    added services to Success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -5,39 +5,49 @@ import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
 
 function Success() {
-  const [addOrder] = useMutation(ADD_ORDER);
+	const [addOrder] = useMutation(ADD_ORDER);
 
-  useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
+	useEffect(() => {
+		async function saveOrder() {
+			const cart = await idbPromise('cart', 'get');
+			const products = cart.map((item) => item._id);
+			const services = cart.map((item) => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
+			if (products.length) {
+				const { data } = await addOrder({ variables: { products } });
+				const productData = data.addOrder.products;
 
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
+				productData.forEach((item) => {
+					idbPromise('cart', 'delete', item);
+				});
+			}
 
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
-    }
+			if (services.length) {
+				const { data } = await addOrder({ variables: { services } });
+				const serviceData = data.addOrder.cervices;
 
-    saveOrder();
-  }, [addOrder]);
+				serviceData.forEach((item) => {
+					idbPromise('cart', 'delete', item);
+				});
+			}
 
-  return (
-    <div>
-      <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
-      </Jumbotron>
-    </div>
-  );
+			setTimeout(() => {
+				window.location.assign('/');
+			}, 3000);
+		}
+
+		saveOrder();
+	}, [addOrder]);
+
+	return (
+		<div>
+			<Jumbotron>
+				<h1>Success!</h1>
+				<h2>Thank you for your purchase!</h2>
+				<h2>You will now be redirected to the home page</h2>
+			</Jumbotron>
+		</div>
+	);
 }
 
 export default Success;
