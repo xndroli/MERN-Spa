dcd8597478abc01f313e9084abe2366080265810
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-15 15:08:20 -0400

    added Service to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,60 +1,84 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
+	type Category {
+		_id: ID
+		name: String
+	}
 
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
+	type Product {
+		_id: ID
+		name: String
+		description: String
+		image: String
+		quantity: Int
+		price: Float
+		category: Category
+	}
 
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
+	type Service {
+		_id: ID
+		name: String
+		description: String
+		image: String
+		quantity: Int
+		price: Float
+		category: Category
+	}
 
-  type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
+	type Order {
+		_id: ID
+		purchaseDate: String
+		products: [Product]
+		services: [Service]
+	}
 
-  type Checkout {
-    session: ID
-  }
+	type User {
+		_id: ID
+		firstName: String
+		lastName: String
+		email: String
+		orders: [Order]
+	}
 
-  type Auth {
-    token: ID
-    user: User
-  }
+	type Checkout {
+		session: ID
+	}
 
-  type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
-    user: User
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
-  }
+	type Auth {
+		token: ID
+		user: User
+	}
 
-  type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
-    login(email: String!, password: String!): Auth
-  }
+	type Query {
+		categories: [Category]
+		products(category: ID, name: String): [Product]
+		product(_id: ID!): Product
+		services(category: ID, name: String): [Service]
+		service(_id: ID!): Service
+		user: User
+		order(_id: ID!): Order
+		checkout(products: [ID]!): Checkout
+	}
+
+	type Mutation {
+		addUser(
+			firstName: String!
+			lastName: String!
+			email: String!
+			password: String!
+		): Auth
+		addOrder(products: [ID]!, services: [ID]!): Order
+		updateUser(
+			firstName: String
+			lastName: String
+			email: String
+			password: String
+		): User
+		updateProduct(_id: ID!, quantity: Int!): Product
+		updateService(_id: ID!, quantity: Int!): Service
+		login(email: String!, password: String!): Auth
+	}
 `;
 
 module.exports = typeDefs;
