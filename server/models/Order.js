d00eb47a83b99fcb0ae18ecb2281d21ebe19741b
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-15 15:11:50 -0400

    added services to server model Order

--- server/models/Order.js
+++ server/models/Order.js
@@ -3,16 +3,22 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-  purchaseDate: {
-    type: Date,
-    default: Date.now
-  },
-  products: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Product'
-    }
-  ]
+	purchaseDate: {
+		type: Date,
+		default: Date.now,
+	},
+	products: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Product',
+		},
+	],
+	services: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Service',
+		},
+	],
 });
 
 const Order = mongoose.model('Order', orderSchema);
