ad744f3ef6dee323b122a79ef9adbac953614c32
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-09-19 19:57:03 -0400

    updated models, working on seeds

---
+++ server/models/Service.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const serviceSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+});
+
+const Service = mongoose.model("Service", serviceSchema);
+
+module.exports = Service;
--- server/models/User.js
+++ server/models/User.js
@@ -53,6 +53,6 @@ const userSchema = new Schema({
 //   return await bcrypt.compare(password, this.password);
 // };
 
-// const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-// module.exports = User;
+module.exports = User;
